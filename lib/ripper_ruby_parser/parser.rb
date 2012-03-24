3728b99d5653f6938f4b5655d736742a0d0c691e
Matijs van Zuijlen <matijs@matijs.net>
2012-03-24 17:12:13 +0100

    Use ripper-plus to prepare ripper's output.
    This should make ripper_ruby_parser work properly on Ruby 1.9.2.

--- lib/ripper_ruby_parser/parser.rb
+++ lib/ripper_ruby_parser/parser.rb
@@ -1,5 +1,6 @@
 require 'ripper_ruby_parser/commenting_sexp_builder'
 require 'ripper_ruby_parser/sexp_processor'
+require 'ripper-plus'
 
 module RipperRubyParser
   # Main parser class. Brings together Ripper and our
@@ -11,7 +12,8 @@ module RipperRubyParser
 
     def parse source, filename='(string)', lineno=1
       parser = CommentingSexpBuilder.new(source, filename, lineno)
-      exp = Sexp.from_array(parser.parse)
+      arr = RipperPlus.for_ripper_ast(parser.parse)
+      exp = Sexp.from_array(arr)
       @processor.filename = filename
       @processor.process exp
     end
